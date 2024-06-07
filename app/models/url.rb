f8e7e4725d0855658111ac7a64308ba55f2bf2d7
david30-maker <davidokpah396@gmail.com>
2024-06-07 18:25:38 +0100

    create validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,19 @@
 class Url < ApplicationRecord
+    validates :original_url, presence: true, formate: { with: URI::DEFAULT_PARSER.make_regexp}, uniqueness: true
+    validates :short_url, uniqueness: true
+
+    before_create :generate_short_url, :initialize_click_count
+
+    private
+
+    def generate_short_url
+        self.short_url = loop do
+            token = SecureRandom.urlsafe_base64(6)
+            break token unless Url.exists?(short_url: token)
+        end
+    end
+
+    def initialize_click_count
+        self.click_count = 0
+    end
 end
