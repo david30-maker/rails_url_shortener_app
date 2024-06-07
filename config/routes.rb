8190bb0c5fc283d11df6ddcccc70f1aa0e565578
david30-maker <davidokpah396@gmail.com>
2024-06-07 18:34:33 +0100

    add route for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "urls#new"
   resources :urls, only: [:new, :create, :show]
-  get "/:short_url", to: "urls#redirect" as: :short
+  get "/:short_url", to: "urls#redirect", as: :short
 end
